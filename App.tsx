8c0b4cdc80c669b752a668f5e7284773d0b87a2a
Juwon Jeong <2jw0718@gmail.com>
2024-10-22 16:45:44 +0900

    플렉스 가중치를 나누어 갖는 뷰 추가

--- App.tsx
+++ App.tsx
@@ -1,11 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: "red",
+        }}
+      />
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: "green",
+        }}
+      />
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: "blue",
+        }}
+      />
     </View>
   );
 }
@@ -13,8 +28,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
 });
