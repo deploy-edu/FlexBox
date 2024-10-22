2f701ef350cf1df56068ba15d66b68d3537b6ac4
Juwon Jeong <2jw0718@gmail.com>
2024-10-22 17:20:37 +0900

    고정된 높이와 가중치적용

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import { StyleSheet, View } from "react-native";
 
+const DEFAULT_HEIGHT = 100;
+
 export default function App() {
+  const [topHeight, setTopHeight] = useState(DEFAULT_HEIGHT);
+  const [bottomHeight, setBottomHeight] = useState(DEFAULT_HEIGHT);
+
   return (
     <View style={styles.container}>
       <View
         style={{
-          flex: 1,
+          height: topHeight,
           backgroundColor: "red",
         }}
       />
@@ -17,7 +23,7 @@ export default function App() {
       />
       <View
         style={{
-          flex: 1,
+          height: bottomHeight,
           backgroundColor: "blue",
         }}
       />
